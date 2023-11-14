c2af38eb8c283ffa116aa41b9bc7e17833530b0b
Khales Rahman <mrahman10@ggc.edu>
2023-11-14 15:08:21 -0500

    React Api demonstration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+// eslint-disable-next-line
+import { User } from './User.js';
+import { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask]= useState("")
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const newTodoList = [...todoList, newTask];
+    setTodoList(newTodoList);
+  };
+
+  const deletTask = (taskName) => {
+    setTodoList()
+  };
+
+  return ( 
+    <div className="App"> 
+      <div className="addTask">
+        <input onChange={handleChange}/>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="list">
+        {todoList.map((task)=> {
+          return (
+          <div>
+            <h1>{task}</h1>
+            <button onClick={() => deleteTask(task)}>X</button>
+          </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
 export default App;
+ 
